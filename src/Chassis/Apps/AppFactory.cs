f5517908ebadde9bda56bd029758b7d21c294e8f
Dru Sellers <dru@drusellers.com>
2016-09-30 07:50:19 -0500

    Upgrade Autofac + Multitenant

--- src/Chassis/Apps/AppFactory.cs
+++ src/Chassis/Apps/AppFactory.cs
@@ -4,7 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
 using Autofac;
-using Autofac.Extras.Multitenant;
+using Autofac.Multitenant;
 using Chassis.Features;
 using Chassis.Tenants;
 using Chassis.Types;
--- src/Chassis/Startup/StartupBootstrapper.cs
+++ src/Chassis/Startup/StartupBootstrapper.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.Web.Http;
+using Chassis.Apps;
 
 namespace Chassis.Startup
 {
